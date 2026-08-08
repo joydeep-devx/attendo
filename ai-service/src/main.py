dbc0309c2ea5574c16bfd37c42c6e4ffe3ea9e80
Beastyy69 <mohitshaw236@gmail.com>
2026-08-09 02:12:55 +0530

    Updated Face Recognization

--- ai-service/src/main.py
+++ ai-service/src/main.py
@@ -2,3 +2,15 @@ import opencv2 as cv
 import numpy as np
 
 cam = cv.VideoCapture(0)
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        break
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow('Video Feed', gray)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+    
