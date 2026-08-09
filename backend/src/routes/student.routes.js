0db8972f9f6cbdae42e77fa7539a28f2d3e003aa
joydeep-devx <sahajoydeep778@gmail.com>
2026-08-09 12:50:40 +0530

    feat: complete student management api

--- backend/src/routes/student.routes.js
+++ backend/src/routes/student.routes.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { createStudent , registerFace } = require("../controllers/student.controller");
+const { createStudent , registerFace ,  getStudents ,getStudent , updateStudent , deleteStudent} = require("../controllers/student.controller");
 
 const router = express.Router();
 
+
+
+
+
 router.post("/", createStudent);
+router.get("/", getStudents);
+router.get("/:id", getStudent);
+router.patch("/:id", updateStudent);
+router.delete("/:id", deleteStudent);
 router.post("/:id/face", registerFace);
 
 module.exports = router;
