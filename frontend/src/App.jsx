9bfdc3061efa50b820888d56076b2c5f41a4e7cb
joydeep-devx <sahajoydeep778@gmail.com>
2026-08-11 20:49:56 +0530

    chore: add motion for page transitions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import MainLayout from './layouts/MainLayout'
 import AppRoutes from './routes/AppRoutes'
+import { motion } from 'motion/react'
 
 function App() {
   return (
     <MainLayout>
+      <motion.div
+        initial={{ opacity: 0, y: 8 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.2 }}
+      ></motion.div>
       <AppRoutes />
     </MainLayout>
   )
